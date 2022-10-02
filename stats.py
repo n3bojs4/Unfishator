099ce241204a1b49bf6109353accc87dee53fb27
n3bojs4 <n3bojs4@gmail.com>
2022-10-02 22:58:51 +0200

    add json output

--- stats.py
+++ stats.py
@@ -3,6 +3,7 @@ import argparse
 import chess
 import chess.pgn
 import chess.engine
+import json
 import sys
 
 
@@ -14,10 +15,85 @@ ChessGames = []
 
 
 
+def OutputAsJson(chessgame,WhitePoints,BlackPoints,WhiteMoves,BlackMoves):
+    Event = chessgame.headers["Event"]
+    Site = chessgame.headers["Site"]
+    Date = chessgame.headers["Date"]
+    EventRound = int(chessgame.headers["Round"])
+    WhitePlayer = chessgame.headers["White"]
+    BlackPlayer = chessgame.headers["Black"]
+    WhiteElo = int(chessgame.headers["WhiteElo"])
+    BlackElo = int(chessgame.headers["BlackElo"])
+    PlyCount = int(chessgame.headers["PlyCount"])
+    WhitePrecision = round((WhitePoints/WhiteMoves)*100,2)
+    BlackPrecision = round((BlackPoints/BlackMoves)*100,2)
+
+
+    # We create the dictionnary
+    GameRecord = {
+        "Event": Event,
+        "Site": Site,
+        "Date": Date,
+        "Round": EventRound,
+        "White": WhitePlayer,
+        "Black": BlackPlayer,
+        "WhiteElo": WhiteElo,
+        "BlackElo": BlackElo,
+        "PlyCount": PlyCount,
+        "WhitePrecision": WhitePrecision,
+        "BlackPrecision": BlackPrecision,
+        "StockFishDepth": depth }
+    # Serializing json
+    json_game = json.dumps(GameRecord, indent=4)    
+
+    print(json_game)
+
+def OutputAsTxt(chessgame,WhitePoints,BlackPoints,WhiteMoves,BlackMoves):
+    Event = chessgame.headers["Event"]
+    Site = chessgame.headers["Site"]
+    Date = chessgame.headers["Date"]
+    EventRound = int(chessgame.headers["Round"])
+    WhitePlayer = chessgame.headers["White"]
+    BlackPlayer = chessgame.headers["Black"]
+    WhiteElo = int(chessgame.headers["WhiteElo"])
+    BlackElo = int(chessgame.headers["BlackElo"])
+    PlyCount = int(chessgame.headers["PlyCount"])
+    WhitePrecision = round((WhitePoints/WhiteMoves)*100,2)
+    BlackPrecision = round((BlackPoints/BlackMoves)*100,2)
+
+    if (WhiteMoves+BlackMoves)==PlyCount:
+        print("Game Informations")
+        print("-----------------------")
+        print("Event:",Event)
+        print("Site:",Site)
+        print("Date:",Date)
+        print("Round:",EventRound)
+        print("White:",WhitePlayer)
+        print("Black:",chessgame.headers["Black"])
+        print("Date:",chessgame.headers["Date"])
+        print("White ELO:",WhiteElo)
+        print("Black ELO:",BlackElo)
+        print("Moved Played:",PlyCount)
+        print("Whitepoints=",WhitePoints)
+        print("Blackpoints=",BlackPoints)
+        print("Statistics of the game:")
+        print("Stockfish depth: ",depth)
+        print("White precision:",WhitePrecision,"%")
+        print("Black precision:",BlackPrecision,"%")
+        print("-----------------------")
+        print(" ")
+    else:
+        print("Error, the number of calculated moves is not good!")
+
+
+
+
+
 parser = argparse.ArgumentParser(description='Analyze pgn file with stockfish.')
 parser.add_argument('--depth',default=12,type=int, help='Stockfish depth to use.')
-# to implement : arser.add_argument('--log',default='analyze.log',type=str, help='Reporting file path.')
+# to implement : parser.add_argument('--log',default='analyze.log',type=str, help='Reporting file path.')
 parser.add_argument('--file',type=str, help='PGN file path to analyze.')
+parser.add_argument('--format',type=str, help='format for output: txt,json.')
 parser.add_argument('--debug',action='store_true', help='active debug mode.')
 
 args = parser.parse_args()
@@ -28,6 +104,8 @@ if not (args.file): parser.error("Please give a pgn file with --file.")
 depth = args.depth
 pgnfile = args.file
 debug = args.debug
+format = args.format
+
 
 # Load Stockfish
 engine = chess.engine.SimpleEngine.popen_uci("stockfish")
@@ -38,9 +116,9 @@ if debug:
 try:
     with open(pgnfile) as pgn:
         while True:
-            game = chess.pgn.read_game(pgn)
-            ChessGames.append(game)
-            if game is None:
+            Round = chess.pgn.read_game(pgn)
+            ChessGames.append(Round)
+            if Round is None:
                 # We are at the end of the file and we delete the last list element which is None
                 del ChessGames[-1]
                 break
@@ -48,7 +126,7 @@ except:
     print("Cannot open pgn file:",pgnfile)
     sys.exit()
 
-for games in ChessGames:
+for game in ChessGames:
 
     # Reset board
     board.reset()
@@ -61,7 +139,7 @@ for games in ChessGames:
     BlackPoints = 0
 
 
-    for move in games.mainline_moves():
+    for move in game.mainline_moves():
         # Set new position
         board.push(move)
 
@@ -90,19 +168,10 @@ for games in ChessGames:
             BlackNextBestMove = engine.play(board, chess.engine.Limit(depth=depth))
     
         counter=counter+1
-
-    print("Game Informations")
-    print("-----------------")
-    print("Event:",games.headers["Event"])
-    print("White:",games.headers["White"])
-    print("Black:",games.headers["Black"])
-    print("Date:",games.headers["Date"])
-    print("Whitepoints=",WhitePoints)
-    print("Blackpoints=",BlackPoints)
-    print("Statistics of the game:")
-    print("Stockfish depth: ",depth)
-    print("White:",round((WhitePoints/WhiteMoves)*100,2),"%")
-    print("Black:",round((BlackPoints/BlackMoves)*100,2),"%")
+    if "txt" in format:
+        OutputAsTxt(game,WhitePoints,BlackPoints,WhiteMoves,BlackMoves)
+    if "json" in format:
+        OutputAsJson(game,WhitePoints,BlackPoints,WhiteMoves,BlackMoves)
 
 engine.quit()
 
